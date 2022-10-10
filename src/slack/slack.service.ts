80944e4b2b5ab48e6cc533c1c15306dc04185ec4
scw3812 <scw3812@naver.com>
2022-10-10 18:30:14 +0900

    :art: slack view 분리

--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -1,5 +1,6 @@
 import { HttpException, Inject, Injectable } from '@nestjs/common';
 import type { ConfigType } from '@nestjs/config';
+import type { ModalView, Block, KnownBlock, MessageAttachment } from '@slack/web-api';
 import { WebClient } from '@slack/web-api';
 import slackConfig from 'src/config/slack.config';
 
@@ -11,6 +12,115 @@ export class SlackService {
   private readonly LINK_ACTION_ID = 'link';
   private readonly CONTENT_ACTION_ID = 'contents';
 
+  private modalView: ModalView = {
+    type: 'modal',
+    title: {
+      type: 'plain_text',
+      text: 'Zettel',
+      emoji: true,
+    },
+    submit: {
+      type: 'plain_text',
+      text: '제출',
+      emoji: true,
+    },
+    close: {
+      type: 'plain_text',
+      text: '닫기',
+    },
+    callback_id: 'call_modal',
+    blocks: [
+      {
+        type: 'input',
+        element: {
+          type: 'multi_users_select',
+          placeholder: {
+            type: 'plain_text',
+            text: 'Select companions',
+            emoji: true,
+          },
+          action_id: this.USER_ACTION_ID,
+        },
+        label: {
+          type: 'plain_text',
+          text: '동료',
+          emoji: true,
+        },
+      },
+      {
+        type: 'input',
+        element: {
+          type: 'multi_static_select',
+          placeholder: {
+            type: 'plain_text',
+            text: 'Select options',
+            emoji: true,
+          },
+          options: [
+            {
+              text: {
+                type: 'plain_text',
+                text: '태그1',
+                emoji: true,
+              },
+              value: 'value-0',
+            },
+            {
+              text: {
+                type: 'plain_text',
+                text: '태그2',
+                emoji: true,
+              },
+              value: 'value-1',
+            },
+            {
+              text: {
+                type: 'plain_text',
+                text: '태그3',
+                emoji: true,
+              },
+              value: 'value-2',
+            },
+          ],
+          action_id: this.TAG_ACTION_ID,
+        },
+        label: {
+          type: 'plain_text',
+          text: 'Label',
+          emoji: true,
+        },
+      },
+      {
+        type: 'divider',
+      },
+      {
+        type: 'input',
+        element: {
+          type: 'plain_text_input',
+          action_id: this.LINK_ACTION_ID,
+        },
+        label: {
+          type: 'plain_text',
+          text: '링크',
+          emoji: true,
+        },
+      },
+      {
+        type: 'input',
+        element: {
+          type: 'plain_text_input',
+          multiline: true,
+          action_id: this.CONTENT_ACTION_ID,
+        },
+        label: {
+          type: 'plain_text',
+          text: '내용',
+          emoji: true,
+        },
+      },
+    ],
+  };
+
   constructor(@Inject(slackConfig.KEY) private config: ConfigType<typeof slackConfig>) {
     this.slack = new WebClient(config.botToken);
   }
@@ -18,114 +128,7 @@ export class SlackService {
   async callModal(trigger_id) {
     const result = await this.slack.views.open({
       trigger_id,
-      view: {
-        type: 'modal',
-        title: {
-          type: 'plain_text',
-          text: 'Zettel',
-          emoji: true,
-        },
-        submit: {
-          type: 'plain_text',
-          text: '제출',
-          emoji: true,
-        },
-        close: {
-          type: 'plain_text',
-          text: '닫기',
-        },
-        callback_id: 'call_modal',
-        blocks: [
-          {
-            type: 'input',
-            element: {
-              type: 'multi_users_select',
-              placeholder: {
-                type: 'plain_text',
-                text: 'Select companions',
-                emoji: true,
-              },
-              action_id: this.USER_ACTION_ID,
-            },
-            label: {
-              type: 'plain_text',
-              text: '동료',
-              emoji: true,
-            },
-          },
-          {
-            type: 'input',
-            element: {
-              type: 'multi_static_select',
-              placeholder: {
-                type: 'plain_text',
-                text: 'Select options',
-                emoji: true,
-              },
-              options: [
-                {
-                  text: {
-                    type: 'plain_text',
-                    text: '태그1',
-                    emoji: true,
-                  },
-                  value: 'value-0',
-                },
-                {
-                  text: {
-                    type: 'plain_text',
-                    text: '태그2',
-                    emoji: true,
-                  },
-                  value: 'value-1',
-                },
-                {
-                  text: {
-                    type: 'plain_text',
-                    text: '태그3',
-                    emoji: true,
-                  },
-                  value: 'value-2',
-                },
-              ],
-              action_id: this.TAG_ACTION_ID,
-            },
-            label: {
-              type: 'plain_text',
-              text: 'Label',
-              emoji: true,
-            },
-          },
-          {
-            type: 'divider',
-          },
-          {
-            type: 'input',
-            element: {
-              type: 'plain_text_input',
-              action_id: this.LINK_ACTION_ID,
-            },
-            label: {
-              type: 'plain_text',
-              text: '링크',
-              emoji: true,
-            },
-          },
-          {
-            type: 'input',
-            element: {
-              type: 'plain_text_input',
-              multiline: true,
-              action_id: this.CONTENT_ACTION_ID,
-            },
-            label: {
-              type: 'plain_text',
-              text: '내용',
-              emoji: true,
-            },
-          },
-        ],
-      },
+      view: this.modalView,
     });
 
     return result ? true : false;
@@ -133,7 +136,6 @@ export class SlackService {
 
   async getModalValues(payload) {
     const blocks = Object.values(payload.view.state.values);
-    console.log(blocks);
 
     const values = new Map();
     for (let i = 0; i < blocks.length; i++) {
@@ -149,8 +151,6 @@ export class SlackService {
     const link = values.get(this.LINK_ACTION_ID).value;
     const content = values.get(this.CONTENT_ACTION_ID).value;
 
-    console.log(tags);
-
     const data = { user, receiveUsers, tags, link, content };
     const receiverMentions = receiveUsers.map((user) => `<@${user}>`).join(' ');
 
@@ -163,48 +163,52 @@ export class SlackService {
       throw new HttpException('no channels', 400);
     }
 
+    const messageBlocks: (Block | KnownBlock)[] = [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `*${receiverMentions}님! 이거 같이 볼까요?*`,
+        },
+      },
+      {
+        type: 'divider',
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `<@${user.id}>님이 공유했어요`,
+        },
+      },
+    ];
+
+    const messageAttachments: MessageAttachment[] = [
+      {
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `${tags.join(' ')}`,
+            },
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `${content} \n<${link}|읽어보기>`,
+            },
+          },
+        ],
+      },
+    ];
+
     await this.slack.chat.postMessage({
       channel: channels[0].id,
       text: 'test',
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `*${receiverMentions}님! 이거 같이 볼까요?*`,
-          },
-        },
-        {
-          type: 'divider',
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `<@${user.id}>님이 공유했어요`,
-          },
-        },
-      ],
-      attachments: [
-        {
-          blocks: [
-            {
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: `${tags.join(' ')}`,
-              },
-            },
-            {
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: `${content} \n<${link}|읽어보기>`,
-              },
-            },
-          ],
-        },
-      ],
+      blocks: messageBlocks,
+      attachments: messageAttachments,
     });
 
     return data ? true : false;
